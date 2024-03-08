fedc638909ff15cab89f188c300acf5473d1f86f
akarreg <161454874+akarreg@users.noreply.github.com>
2024-03-08 13:38:14 +0100

    java city search

--- java.js
+++ java.js
@@ -1,1 +1,10 @@
-alert("hi");
+function infoSearch(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#search-city");
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = searchCity.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+
+searchFormElement.addEventListener("submit", infoSearch);
