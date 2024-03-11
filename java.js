b6e9e4db8c7eeb224ae035d6727e0980aa44cca2
akarreg <161454874+akarreg@users.noreply.github.com>
2024-03-11 12:50:20 +0100

    invalid apikey

--- java.js
+++ java.js
@@ -1,13 +1,16 @@
-function infoSearch(event) {
-  event.preventDefault();
-  let searchCity = document.querySelector("#search-city");
-  let cityName = document.querySelector("#city-name");
-  cityName.innerHTML = searchCity.value;
+function searchCity(city) {
+  let apiKey = "bc0c992ff01fe3156bt9ead9dob31418";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
 }
 
+function citySubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city");
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
 let searchFormElement = document.querySelector("#search-form");
-
-searchFormElement.addEventListener("submit", infoSearch);
-
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={citi.name}&key={bc0c992ff01fe3156bt9ead9dob31418}";
+searchFormElement.addEventListener("submit", citySubmit);
