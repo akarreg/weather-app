2bba3079bfda96d67ac25b7f26ba7a32dff51745
akarreg <161454874+akarreg@users.noreply.github.com>
2024-03-12 10:43:30 +0100

    search temp.

--- java.js
+++ java.js
@@ -1,15 +1,23 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "bc0c992ff01fe3156bt9ead9dob31418";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function citySubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", citySubmit);
+searchCity("Bilbao");
